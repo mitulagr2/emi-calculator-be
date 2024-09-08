22a0dbc7b6a178c380908038e0156d9a7bfd1d79
mitulagr2 <mitulagr2@gmail.com>
2024-09-08 17:34:36 +0530

    add EMI model
    - id (primary key, auto-increment)
    - loan_amount (decimal)
    - interest_rate (decimal)
    - loan_tenure_months (integer)
    - emi (decimal)
    prepayment_amount (decimal, default null) - stores any extra payments made during the loan.
    - remaining_balance (decimal)

---
+++ src/models/emi.ts
@@ -0,0 +1,45 @@
+import {
+  DataTypes,
+  Model,
+  Sequelize,
+  type CreationOptional,
+  type InferAttributes,
+  type InferCreationAttributes,
+} from "sequelize";
+
+class EMI extends Model<InferAttributes<EMI>, InferCreationAttributes<EMI>> {
+  declare id: number;
+  declare loan_amount: number;
+  declare interest_rate: number;
+  declare loan_tenure_months: number;
+  declare emi: number;
+  declare prepayment_amount: CreationOptional<number>;
+  declare remaining_balance: number;
+}
+
+const init = (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
+  EMI.init(
+    {
+      id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      loan_amount: { type: dataTypes.DECIMAL, allowNull: false },
+      interest_rate: { type: dataTypes.DECIMAL, allowNull: false },
+      loan_tenure_months: { type: dataTypes.INTEGER, allowNull: false },
+      emi: { type: dataTypes.DECIMAL, allowNull: false },
+      prepayment_amount: { type: dataTypes.DECIMAL, defaultValue: null },
+      remaining_balance: { type: dataTypes.DECIMAL, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "EMI",
+      timestamps: true,
+    }
+  );
+  return EMI;
+};
+
+export default init;
